68dde575fab83b3be3aa3d9d6fd360095005b1af
Gabriel Santos <gabriel.santos@fluencypass.com>
2022-09-09 19:30:23 -0300

    Palindrome

--- data-structures/deque.js
+++ data-structures/deque.js
@@ -124,3 +124,42 @@ console.log(deque.toString()); // Jack
 deque.addFront("John"); // John comes back for information.
 
 console.log(deque.toString()); // John, Jack
+
+// Real life problems with Deque Class
+
+function palindromeChecker(aString) {
+  if (!aString || !aString.trim()) return false;
+
+  const deque = new Deque();
+  const lowerString = aString.toLocaleLowerCase().split("").join("");
+
+  let isEqual = true;
+  let firstChar, lastChar;
+
+  for (let i = 0; i < lowerString.length; i++) {
+    deque.addBack(lowerString.charAt(i));
+  }
+
+  while (deque.size() > 1 && isEqual) {
+    firstChar = deque.removeFront();
+    lastChar = deque.removeBack();
+
+    if (firstChar !== lastChar) {
+      isEqual = false;
+    }
+  }
+
+  return isEqual;
+}
+
+// All outputs will be true
+
+console.log("a", palindromeChecker("a"));
+console.log("aa", palindromeChecker("aa"));
+console.log("kayak", palindromeChecker("kayak"));
+console.log("level", palindromeChecker("level"));
+console.log(
+  "Was it a car or a cat I saw",
+  palindromeChecker("Was it a car or a cat I saw")
+);
+console.log("Step on no pets", palindromeChecker("Step on no pets"));
