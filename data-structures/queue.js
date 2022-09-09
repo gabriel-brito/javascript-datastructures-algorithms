b5c22a750b9e82c8e85bf11146614b6d2d08229c
Gabriel Santos <gabriel.santos@fluencypass.com>
2022-09-09 19:19:16 -0300

    hot potato with Queue Data Structure

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -77,3 +77,36 @@ queue.dequeue();
 queue.dequeue();
 
 console.log(queue.toString()); // 'Camila'
+
+// Real life problems with Queue Class
+
+function hotPotato(personsList, num) {
+  const queue = new Queue();
+  const eliminatedList = [];
+
+  for (let i = 0; i < personsList.length; i++) {
+    queue.enqueue(personsList[i]);
+  }
+
+  while (queue.size() > 1) {
+    for (let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+
+    eliminatedList.push(queue.dequeue());
+  }
+
+  return {
+    eliminated: eliminatedList,
+    winner: queue.dequeue(),
+  };
+}
+
+const names = ["John", "Jack", "Camila", "Ingrid", "Carl"];
+const result = hotPotato(names, 7);
+
+result.eliminated.forEach((name) =>
+  console.log(`${name} was eliminated from the Hot Potato game.`)
+);
+
+console.log(`The winner is: ${result.winner}`);
