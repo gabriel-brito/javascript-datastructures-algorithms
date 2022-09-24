8c9208537b17e6716a26ddbb6513c6e7458703e0
Gabriel Santos <mfkrgabriel@gmail.com>
2022-09-24 17:04:59 -0300

    Circular linked list

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -221,3 +221,66 @@ class DoublyLinkedList extends LinkedList {
     return undefined;
   }
 }
+
+// --------------------
+
+class CircularLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    super(equalsFn);
+  }
+
+  insert(element, index) {
+    if ((index) => 0 && index <= this.count) {
+      const node = new Node(element);
+      let current = this.head;
+
+      if (index === 0) {
+        if (this.head === null) {
+          this.head = node;
+          node.next = this.head; // New
+        } else {
+          node.next = current;
+          current = this.getElementAt(this.size());
+          // update last element
+          this.head = node;
+          current.next = this.head;
+        }
+      } else {
+        const previous = this.getElementAt(index - 1);
+        node.next = previous.next;
+        previous.next = node;
+      }
+
+      this.count++;
+      return true;
+    }
+  }
+
+  remoteAt(index) {
+    if ((index) => 0 && index <= this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        if (this.size() === 1) {
+          this.head = undefined;
+        } else {
+          const removed = this.head;
+          current = this.getElementAt(this.size()); // new
+          this.head = this.head.next;
+          current.next = this.head;
+          current = removed; // Just to return the element removed
+        }
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+      }
+
+      this.count--;
+
+      return current.element;
+    }
+
+    return undefined;
+  }
+}
