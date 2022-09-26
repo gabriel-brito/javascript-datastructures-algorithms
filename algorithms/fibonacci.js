5f4eb5930c4da2a694c8c69c3c6b947bd0ace19b
Gabriel Santos <mfkrgabriel@gmail.com>
2022-09-26 20:21:54 -0300

    Fibonacci

---
+++ algorithms/fibonacci.js
@@ -0,0 +1,21 @@
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144....
+// the pattern of the sequence is that each value is the sum of the 2 previous values,
+//that means that for N=5 -> 2 + 3
+
+function fibonacciIterative(n) {
+  if (n < 2) return n;
+
+  let result = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    result[i] = result[i - 2] + result[i - 1];
+  }
+
+  return result[result.length - 1];
+}
+
+function fibonacciRecursive(n) {
+  if (n <= 1) return n;
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
