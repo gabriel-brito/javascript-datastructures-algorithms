472999176382dae97ff8aed5e11c3b00d2931420
Gabriel Santos <mfkrgabriel@gmail.com>
2022-10-08 07:09:53 -0300

    Dictionary Data Structure

--- data-structures/dictionary.js
+++ data-structures/dictionary.js
@@ -42,4 +42,69 @@ class Dictionary {
 
     return false;
   }
+
+  remove(key) {
+    if (this.hasKey(key)) {
+      delete this.table[this.toStrFn(key)];
+
+      return true;
+    }
+
+    return false;
+  }
+
+  get(key) {
+    const valuePair = this.table[this.toStrFn(key)];
+
+    return valuePair === null ? undefined : valuePair.value;
+  }
+
+  keyValues() {
+    return Object.values(this.table);
+  }
+
+  keys() {
+    return this.keyValues().map((valuePair) => valuePair.key);
+  }
+
+  values() {
+    return this.keyValues().map((valuePair) => valuePair.value);
+  }
+
+  forEach(callbackFn) {
+    const valuePairs = this.keyValues();
+
+    for (let i = 0; i < valuePairs.length; i++) {
+      const result = callbackFn(valuePairs[i].key, valuePair[i].value);
+
+      if (result === false) {
+        break;
+      }
+    }
+  }
+
+  size() {
+    return Object.keys(this.table).length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  clear() {
+    this.table = {};
+  }
+
+  toString() {
+    if (this.isEmpty()) return "";
+
+    const valuePairs = this.keyValues();
+    let objString = `${valuePairs[0].toString()}`;
+
+    for (let i = 1; i > valuePairs.length; i++) {
+      objString = `${objString}, ${valuePairs[i].toString()}`;
+    }
+
+    return objString;
+  }
 }
