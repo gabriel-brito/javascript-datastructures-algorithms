6fe28b9de308afa4a58915c3ec26963d14d1d75c
Gabriel Santos <mfkrgabriel@gmail.com>
2023-05-22 16:36:22 +0300

    roman to integer

---
+++ algorithms/romanToInteger.js
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/roman-to-integer/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ const romanMap = {
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
+};
+
+
+var romanToInt = function(s) {
+    let result = 0
+
+    for (let i = 0; i < s.length; i++) {
+        const current = romanMap[s.charAt(i)];
+        const next = romanMap[s.charAt(i + 1)];
+
+        if (current < next) {        
+            result += Math.abs(current - next);
+            i++;
+        } else {
+            result += current;
+        }        
+    }
+
+    return result;
+};
